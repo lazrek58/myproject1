f14238aaf53e7a3e25feccd4c65549cf3ab03827
lazrek58 <laz.abdelhamid58@gmail.com>
2026-04-14 22:11:54 +0100

    add test3.py

---
+++ test3.py
@@ -0,0 +1,14 @@
+print("Soit la liste de nombres :\nnombres = [5, 2, 9, 1, 7]")
+nombres = [5, 2, 9, 1, 7]
+print("Trier en place avec  nombres.sort() :")
+nombres.sort()  # Trier en place
+print(nombres)  # [1, 2, 5, 7, 9]
+print("Avec sorted(),il n'ya pas de\n modification de la liste originale.")
+# Avec sorted() (ne modifie pas la liste originale)
+nombres = [5, 2, 9, 1, 7]
+triés = sorted(nombres)
+print(triés)     # [1, 2, 5, 7, 9]
+print("Soit la liste de nombres :")
+print(nombres)   # [5, 2, 9, 1, 7]"
+ 
+
